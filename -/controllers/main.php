c143678f041369cc5507848868671aa90f47879c
fed <platinumaccount@yandex.ru>
2019-02-09 01:32:04 +0300

    rm old

--- -/controllers/main.php
+++ -/controllers/main.php
@@ -1,40 +1,6 @@
 <?php namespace ewma\access\controllers;
 
-// todo удалить. предварительно поменять в роутерах \ewma\access~:hasPermission на \ewma~access:hasPermission
-
 class Main extends \Controller
 {
-    public function isUser()
-    {
-        return $this->_user() ? true : false;
-    }
 
-    public function isGuest()
-    {
-        return $this->_user() ? false : true;
-    }
-
-    public function hasPermission()
-    {
-        return $user = $this->_user() and $user->hasPermission($this, $this->data('path'));
-    }
-
-    public function otherUserHasPermission()
-    {
-        if ($userModel = \ewma\access\models\User::find($this->data('user_id'))) {
-            return $user = $this->app->access->getUser($userModel) and $user->hasPermission($this, $this->data('path'));
-        }
-    }
-
-    public function getUserModelById()
-    {
-        return \ewma\access\models\User::find($this->data('id'));
-    }
-
-    public function logout()
-    {
-        $this->app->access->auth->logout();
-
-        $this->app->response->redirect($this->data('redirect'));
-    }
 }
